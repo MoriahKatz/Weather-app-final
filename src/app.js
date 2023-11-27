0aff6e737a7c39f35296cc1cd8c3c2c751ca008d
MoriahKatz <151691496+MoriahKatz@users.noreply.github.com>
2023-11-27 01:00:55 -0800

    added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "4025a5c5a7f2a55o1f6cc282td1abf37";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSubmit);
+
+searchCity("Benin");
